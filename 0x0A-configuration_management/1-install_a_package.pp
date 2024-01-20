3aa3448fb5cfe763e655637b92dbdb712a1761ee
timmySpark <timmyspark1@gmail.com>
2024-01-19 16:06:57 -0800

    [feat] :- trying another format

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 # Using Puppet, install flask from pip3.
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+
+# installing Werkzeug too , requires flask 
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
