32f1e6d0614a1ef5435456bfec9b83f812f6ff54
timmySpark <timmyspark1@gmail.com>
2024-03-06 00:56:47 +0000

    [feat] :- querying subreddit api, return num of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Module Containing Reddit API and queries'''
+import requests
+
+def number_of_subscribers(subreddit):
+    ''' Queries Subreddit Api, returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit, if subreddit is invalid,function should return Zero(0)
+    '''
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+            }
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 404:
+        return 0
+    results = resp.json().get("data")
+    return results.get("subscribers")
