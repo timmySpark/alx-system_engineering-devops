14ec58c67ac1792b02740dc977f753f46d638495
timmySpark <timmyspark1@gmail.com>
2024-03-07 20:52:58 +0000

    [feat] :- working on errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,11 @@ def number_of_subscribers(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+    }
     resp = requests.get(url, headers=headers, allow_redirects=False)
-    if resp.status_code == 404:
+    if resp.status_code == 200:
+        results = resp.json().get("data")
+        return results.get("subscribers")
+    else:
         return 0
-    results = resp.json().get("data")
-    return results.get("subscribers")
